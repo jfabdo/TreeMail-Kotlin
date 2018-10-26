065bed674955f63d5e9668cc7f1a8e3df4bd47e8
Jack Abdo <jfabdo@gmail.com>
2018-10-26 14:29:54 -0700

    most common words completed

---
+++ Summarize.kt
@@ -0,0 +1,48 @@
+// Written by Jack Abdo
+
+import java.util.*
+import java.io.File
+
+fun mostcommonwords(): List<String> {
+
+val filename: String = "mostcommonwords.txt"
+val mostcommonwords: List<String> = File(filename).readLines()
+return mostcommonwords
+}
+
+fun ranksentences() { // return top summarized sentences 
+
+
+
+}
+
+fun removewhitespace(textstring: String): String {
+
+return " "
+
+}
+
+
+fun sortwords(wordcount: Int): String {
+
+return " "
+
+}
+
+fun wordfreq(textstring: String) {//: HashMap {
+}
+
+fun countwords(textstring: String): Int  {
+
+return 0
+}
+
+fun returntop(wordcount: Int) {
+}
+
+fun countworddensity(article: String,wordcount: Int) {
+}
+
+fun summarize(article: String) {
+}
+
